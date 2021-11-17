8ec9340a1626bf2fc20f4da3901a562ce9d1d162
Stein Lundbeck <lc@lundbeckconsulting.no>
2021-11-17 03:22:22 +0100

    upgrade to .net 6

--- _Build/Creator.js
+++ _Build/Creator.js
@@ -145,4 +145,114 @@ tabs.forEach((tab) => {
 
 	
 
+﻿/*
+    @Author     Stein Lundbeck
+    @Date       29.08.2020
+ */
+
+get("Id", "creatorBarsMenu").onclick((e) => {
+    let bg = document.createElement("div");
+    let closeIcon = document.createElement("i");
+    closeIcon.id = "closeMenuIcon";
+    bg.id = "barsMenuBackground";
+
+    bg.appendChild(closeIcon);
+    document.body.appendChild(bg);
+    bg.classList.add("fade-in");
+});
+
+get("Id", "#burgerMenuBackground").onclick((e) => {
+    closeMenu();
+});
+
+document.body.onclick((e) => {
+    closeMenu();
+});
+
+function closeMenu() {
+    let bg = get("Id", "burgerMenuBackground");
+    bg.classList.add("fade-out");
+    document.body.removeChild(bg);
+}
+
+
+	
+
+﻿$(document).ready(() => {
+    function CloseModalBackground() {
+        $("#modalBackground").fadeOut("fast", function () {
+            $("#modalBackground").remove();
+        });
+    }
+
+    function TriggerCloseModal($modal) {
+        $($modal).trigger("modal:hidden");
+    }
+
+    function DoCloseModal($modal) {
+        $($modal).removeAttr("open");
+        TriggerCloseModal($modal);
+    }
+
+    function CloseModal(e, hideBG = true) {
+        var $modal = $(e.currentTarget).closest("[class^=\"modal-\"]");
+        DoCloseModal($modal);
+
+        if (hideBG) {
+            CloseModalBackground();
+        }
+    }
+
+    function CloseAllModals() {
+        $("[class^=\"modal-\"][open=\"open\"]").each(function () {
+            DoCloseModal(this);
+        });
+
+        CloseModalBackground();
+    }
+
+    $(".show-modal[data-modal]").click(function (e) {
+        var modalId = $(this).data("modal");
+
+        $("#" + modalId).trigger("modal:show");
+    });
+
+    $("[class^=\"modal-\"]").on("modal:show", function (e) {
+        // eslint-disable-next-line consistent-this
+        var $modal = this;
+        var $bg = "<div id=\"modalBackground\"></div>";
+
+        $("body").append($bg);
+
+        $("#modalBackground").fadeIn("slow", function () {
+            $($modal).attr("open", "open");
+            $($modal).trigger("modal:visible");
+
+            $("html, body").animate({
+                scrollTop: 0
+            }, 600);
+        });
+    });
+
+    $(".hide-modal").click(function (e) {
+        CloseModal(e);
+    });
+
+    $("[class^=\"modal-\"]").on("modal:hide", function (e) {
+        CloseModal(e);
+    });
+
+    $(document).on("click", "#modalBackground", function () {
+        CloseAllModals();
+    });
+
+    $(document).on("keydown", function (e) {
+        if (e.keyCode === 27) {
+            CloseAllModals();
+        }
+    });
+});
+
+	
+
 	//# sourceMappingUrl=Creator.js.map
---
+++ _Build/SCRIPT/TagHelper.BarsMenu.js
@@ -0,0 +1,32 @@
+﻿/*
+    @Author     Stein Lundbeck
+    @Date       29.08.2020
+ */
+
+get("Id", "creatorBarsMenu").onclick((e) => {
+    let bg = document.createElement("div");
+    let closeIcon = document.createElement("i");
+    closeIcon.id = "closeMenuIcon";
+    bg.id = "barsMenuBackground";
+
+    bg.appendChild(closeIcon);
+    document.body.appendChild(bg);
+    bg.classList.add("fade-in");
+});
+
+get("Id", "#burgerMenuBackground").onclick((e) => {
+    closeMenu();
+});
+
+document.body.onclick((e) => {
+    closeMenu();
+});
+
+function closeMenu() {
+    let bg = get("Id", "burgerMenuBackground");
+    bg.classList.add("fade-out");
+    document.body.removeChild(bg);
+}
+
+
+	//# sourceMappingUrl=TagHelper.BarsMenu.js.map
